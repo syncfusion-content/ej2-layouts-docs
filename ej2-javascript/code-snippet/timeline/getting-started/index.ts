8819a06ccd18dafcb89a4142e65b52bcd2ef1eb3
Mani <119287057+manikandan-mariappan@users.noreply.github.com>
2024-03-08 17:00:43 +0530

    Revert "docs(865650): Added UG topics for timeline control"

--- ej2-javascript/code-snippet/timeline/getting-started/index.ts
+++
@@ -1,9 +0,0 @@
-import { Timeline } from '@syncfusion/ej2-layouts';
-
-// Initializes the Timeline control
-let timeline: Timeline = new Timeline({
-  items: [{}, {}, {}, {}]
-});
-
-// Render initialized Timeline.
-timeline.appendTo("#timeline");
